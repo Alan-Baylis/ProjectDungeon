218106f73a0ed6e171cf3c59f5a28bf1c1ab10e4
Matt Middlebrook <matt.midi89@gmail.com>
2016-04-07 19:32:18 -0700

    First commit of the room prototypes

---
+++ Assets/Scripts/ProjectDungeon/Models/Maps/RoomPrototype.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace Models.Maps
+{
+  public class RoomPrototype
+  {
+
+    public int Width { get; set; }
+    public int Height { get; set; }
+
+    public Tile[,] Tiles { get; set; }
+
+    public static List<RoomPrototype> CreateTestRoomPrototypes()
+    {
+      var temp = new List<RoomPrototype>();
+
+      var p1 = new RoomPrototype()
+      {
+        Height = 15,
+        Width = 15,
+      };
+      return temp;
+    }
+  }
+}
